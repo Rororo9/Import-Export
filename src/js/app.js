c78ed556f910a7d1b3f0f5c5a7fc52f9e87ff713
“Vadim” <freegasss@gmail.com>
2024-03-15 04:48:27 +1100

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+// TODO: write your code here
+import Game, { GameSavingData as saveGame, readGameSaving as loadGame, writeGameSaving as saveGame } from './game';
+
+const game = new Game();
+game.start();
